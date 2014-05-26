2187588f981b8d3fe9f28a73ba400035eb6cad65
Jenny Lian <jennylian100@gmail.com>
2014-05-25 22:08:55 -0700

    adding the second

---
+++ OOP_Polymorph/src/Animal.java
@@ -0,0 +1,29 @@
+
+public class Animal {
+	private String name = "Animal";
+	public String favFood = "Food";
+	
+	protected final void changeName(String name){
+		this.name = name;
+	}
+	
+	protected final String getName(){
+		return this.name;
+	} 
+	
+	public void eatStuff(){
+		System.out.println("Yum" + favFood);
+	}
+	
+	public void walkAround(){
+		System.out.println(this.name + "walking");
+	}
+	
+	public Animal(){}
+	
+	public Animal(String name, String food){
+		this.name = name;
+		this.favFood = food;
+	}
+	
+}
